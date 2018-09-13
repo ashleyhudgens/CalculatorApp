201466ba884db70413c84eb956a1e8f944aa26c8
ashleyhudgens <39204522+ashleyhudgens@users.noreply.github.com>
2018-09-12 23:01:50 -0500

    Uploading correct UnitTest

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CustomExtension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //unit test to validate 
+            Calculate.CalculateTotal(1, 2);
+        }
+    }
+}
